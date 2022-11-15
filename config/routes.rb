b9150ea65adbcb3e3553ca440b8d95ad41afb882
sharon-odhiambo <sharon.odhiambo100@gmail.com>
2022-11-15 21:34:41 +0300

    use Devise for user login and registration pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "users#index"
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
