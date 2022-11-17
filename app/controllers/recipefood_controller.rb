7324a8e4efd0d892e481be1bffc0f9748ac67ec5
SimbaWANGU <simbawangulu@gmail.com>
2022-11-17 16:03:04 +0300

    fix linter errors

--- app/controllers/recipefood_controller.rb
+++ app/controllers/recipefood_controller.rb
@@ -6,9 +6,9 @@ class RecipefoodController < ApplicationController
 
   def create
     @foods = Food.all
-    @recipe_id = 
-    @recipefood = RecipeFood.new(quantity: params[:quantity], recipe_id: params[:recipe_id], food_id: params[:food_id])
-    p @recipefood
+    @recipe_id =
+      @recipefood = RecipeFood.new(quantity: params[:quantity], recipe_id: params[:recipe_id],
+                                   food_id: params[:food_id])
     if @recipefood.save
       redirect_to "/recipes/#{params[:recipe_id]}", notice: 'Food was successfully created.'
     else
