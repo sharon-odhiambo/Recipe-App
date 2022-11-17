d609f4227549b969b8e080032ca9e3278ba7762d
Eric <tuyishimireericc@gmail.com>
2022-11-17 08:37:21 +0200

    Created inventory

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root "users#index"
   resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :inventories, only: [:index, :show, :new, :create, :destroy] do
+    resources :inventory_food
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
