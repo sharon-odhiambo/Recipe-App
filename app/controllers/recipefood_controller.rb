b1840817bbc354b90bc50f5fbb3878ae3a1e31a1
SimbaWANGU <simbawangulu@gmail.com>
2022-11-17 16:01:47 +0300

    allow user to add food to recipe

---
+++ app/controllers/recipefood_controller.rb
@@ -0,0 +1,18 @@
+class RecipefoodController < ApplicationController
+  def new
+    @foods = Food.all
+    @recipe_id = params[:recipe_id]
+  end
+
+  def create
+    @foods = Food.all
+    @recipe_id = 
+    @recipefood = RecipeFood.new(quantity: params[:quantity], recipe_id: params[:recipe_id], food_id: params[:food_id])
+    p @recipefood
+    if @recipefood.save
+      redirect_to "/recipes/#{params[:recipe_id]}", notice: 'Food was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
