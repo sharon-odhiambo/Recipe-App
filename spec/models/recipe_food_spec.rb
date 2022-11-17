8d389487326edb11e81c6ae3effb0231e2d844ae
SimbaWANGU <simbawangulu@gmail.com>
2022-11-17 17:45:38 +0300

    fix linters

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Recipe, type: :model do
     @user = User.new(name: 'Test', email: 'test@test.com', password: 123_456, password_confirmation: 123_456)
     @user.skip_confirmation!
     @recipe = Recipe.new(name: 'Simbzz', description: 'test',
-      cooking_time: 20, preparation_time: 30,
-      public: true, user_id: @user.id)
+                         cooking_time: 20, preparation_time: 30,
+                         public: true, user_id: @user.id)
     @food = Food.create(name: 'Water', measurement_unit: 'bottle', price: 2)
   end
   it 'should not be valid without quantity' do
